824577f4823aaeeabedf164a26e7466e67251661
luiggi navilys <luigginavilys@msn.com>
2023-10-26 19:09:51 +0000

    Feature complete: Dictionary check of attempt  n°3

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,10 +1,6 @@
-# pylint: disable=missing-docstring
-# pylint: disable=too-few-public-methods
 import random
 import string
-
 import requests
-
 
 
 class Game:
@@ -14,19 +10,22 @@ class Game:
         for _ in range(9):
             self.grid.append(random.choice(string.ascii_uppercase))
 
-    def is_valid(self, word):
-        # [...]
+    def is_valid(self, word: str) -> bool:
+        """Return True if and only if the word is valid, given the Game's grid"""
+        if not word:
+            return False
+        letters = self.grid.copy()  # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
 
-        return self.__check_dictionary(word)
-
-
+        is_english = self.__check_dictionary(word)
+        return is_english
 
     @staticmethod
     def __check_dictionary(word):
         response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
         json_response = response.json()
-        return json_response['found']
-game = Game()
-new_game = Game()
-new_game.grid = list('KWIENFUQW')
-print(new_game.is_valid('tomato'))
+        return json_response["found"]
